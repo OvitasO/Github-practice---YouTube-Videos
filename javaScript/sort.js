2ffce1607aad005feee9036dfcff2898070be0ef
Vitas <Depsterer@gmail.com>
2026-06-11 15:33:07 +0100

    Cleaned the js code for sort buttons. No functionality yet

--- javaScript/sort.js
+++ javaScript/sort.js
@@ -1,36 +1,24 @@
-const sortAllElem = document.getElementById('sort-all');
-const sortShortsElem = document.getElementById('sort-shorts');
-const sortNotViewElem = document.getElementById('sort-!view');
-const sortViewElem = document.getElementById('sort-view');
-const sortVideoElem = document.getElementById('sort-video');
-const sortRecentElem = document.getElementById('sort-uploaded-recently');
-const sortLiveElem = document.getElementById('sort-live');
+const sortButtons = [
+  document.getElementById('sort-all'),
+  document.getElementById('sort-shorts'),
+  document.getElementById('sort-!view'),
+  document.getElementById('sort-view'),
+  document.getElementById('sort-video'),
+  document.getElementById('sort-uploaded-recently'),
+  document.getElementById('sort-live')
+]
 
 function sortSelector(selectedButton) {
-  sortAllElem
-    .classList.remove('active');
-  sortShortsElem
-    .classList.remove('active');
-  sortNotViewElem
-    .classList.remove('active');
-  sortViewElem
-    .classList.remove('active');
-  sortVideoElem
-    .classList.remove('active');
-  sortRecentElem
-    .classList.remove('active');
-  sortLiveElem
-    .classList.remove('active');
+  for (let i = 0; i < sortButtons.length; i++) {
+    sortButtons[i]
+      .classList.remove('active');
+  }
 
   selectedButton
     .classList.add('active');
 }
 
-sortAllElem.addEventListener('click', function() {sortSelector(sortAllElem)});
-sortShortsElem.addEventListener('click', function() {sortSelector(sortShortsElem)});
-sortNotViewElem.addEventListener('click', function() {sortSelector(sortNotViewElem)});
-sortViewElem.addEventListener('click', function() {sortSelector(sortViewElem)});
-sortVideoElem.addEventListener('click', function() {sortSelector(sortVideoElem)});
-sortRecentElem.addEventListener('click', function() {sortSelector(sortRecentElem)});
-sortLiveElem.addEventListener('click', function() {sortSelector(sortLiveElem)});
-
+for (let i = 0; i < sortButtons.length; i++) {
+  sortButtons[i]
+    .addEventListener('click', function() {sortSelector(sortButtons[i])});
+}
